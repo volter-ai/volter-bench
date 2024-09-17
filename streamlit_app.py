784633623c07595693a41a4931310df02fbe1628
yueranyuan <yueranyuan@gmail.com>
2024-09-17 17:02:29 -0400

    saving projects and all logs

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,20 @@ def get_subdirectories(base_dir):
     return [d for d in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, d))]
 
 def load_data(directory):
-    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+    csv_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == 'results.csv':
+                csv_files.append(os.path.join(root, file))
+
     all_data = []
     for file in csv_files:
-        df = pd.read_csv(os.path.join(directory, file))
-        df['file_timestamp'] = datetime.strptime(file.split('_')[-1].split('.')[0], "%Y-%m-%d-%H-%M-%S")
+        df = pd.read_csv(file)
+        # Extract timestamp from the file path
+        timestamp = file.split(os.path.sep)[-2]
+        df['file_timestamp'] = datetime.strptime(timestamp, "%Y-%m-%d-%H-%M-%S")
         all_data.append(df)
+
     return pd.concat(all_data, ignore_index=True) if all_data else pd.DataFrame()
 
 def calculate_success_rates(df):
