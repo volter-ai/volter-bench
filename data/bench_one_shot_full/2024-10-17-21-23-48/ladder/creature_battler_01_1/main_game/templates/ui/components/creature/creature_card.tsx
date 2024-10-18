57d90e52b2977daea85171a1592417cedf09cd26
Andrew Stelmach <andrew@volter.ai>
2024-10-17 21:27:40 -0400

    Update benchmark results

---
+++ data/bench_one_shot_full/2024-10-17-21-23-48/ladder/creature_battler_01_1/main_game/templates/ui/components/creature/creature_card.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import withClickable from "@/lib/withClickable"
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card"
+
+interface CreatureCardProps extends React.ComponentPropsWithoutRef<typeof Card> {
+  uid: string;
+  name: string;
+  imageUrl: string;
+  hp: number;
+  maxHp: number;
+}
+
+let CreatureCard = React.forwardRef<HTMLDivElement, CreatureCardProps>(
+  ({ className, uid, name, imageUrl, hp, maxHp, ...props }, ref) => (
+    <Card ref={ref} className={cn("w-64", className)} uid={uid} {...props}>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <img src={imageUrl} alt={name} className="w-full h-40 object-cover rounded-md" />
+        <div className="mt-4">
+          <p>HP: {hp}/{maxHp}</p>
+          <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-2">
+            <div 
+              className="bg-blue-600 h-2.5 rounded-full" 
+              style={{ width: `${(hp / maxHp) * 100}%` }}
+            ></div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+)
+
+CreatureCard.displayName = "CreatureCard"
+
+CreatureCard = withClickable(CreatureCard)
+
+export { CreatureCard }
